ab162f72cee27a1bdcaa3a8da5af888afbdd8367
ClusterH <mycluster1990@gmail.com>
2021-06-24 16:35:02 +0800

    update inft-create contract address

--- src/contracts/inft-create-nft.ts
+++ src/contracts/inft-create-nft.ts
@@ -1,6 +1,6 @@
 import Web3 from 'web3';
 import abi from '@/abis/inft-create-nft.json';
-const address = '0x3B6eB9fE7817130cb33E5Dd29aF2E50e83F72fd4';
+const address = '0x52b29289DF14c9Ee2c135378c8c9Cd4eDA867BA8';
 
 const web3 = new Web3(Web3.givenProvider);
 const contract = new web3.eth.Contract(abi as any, address);
